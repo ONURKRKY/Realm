c48363c85e6fb823bef6b7dfe5971158d1091a03
Onur <karakayaonur@yahoo.com>
2021-11-19 18:33:59 +0300

    nested prefab

---
+++ Assets/Tile/CoordinateLabeler.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+[ExecuteAlways]
+public class CoordinateLabeler : MonoBehaviour
+{
+
+ TextMeshPro Label;
+ Vector2Int coordinates= new Vector2Int();
+
+
+    void Awake() 
+    {
+    Label=GetComponent<TextMeshPro>();
+        DisplayCoordinates();
+
+    }
+    void Update()
+    {
+        if(!Application.isPlaying)
+        {
+            DisplayCoordinates();
+            UpdateObjectName();
+        }
+    }
+
+     void DisplayCoordinates()
+    {
+        coordinates.x=Mathf.RoundToInt(transform.parent.position.x/UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y=Mathf.RoundToInt(transform.parent.position.z/UnityEditor.EditorSnapSettings.move.z);
+        Label.text=coordinates.x +","+coordinates.y;
+    }
+
+    void UpdateObjectName()
+    {
+        transform.parent.name=coordinates.ToString();
+    }
+}
